9640bf5c7b1be6fc6a89287c96031bfd18a48f6e
Samuelj-santos <samuel.00000847429@unicap.br>
2023-03-10 21:25:08 -0300

    refactor: refactoring

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,42 +3,36 @@ import {
   StyleSheet,
   Text,
   Image,
-  View,
   ScrollView,
 } from 'react-native';
 
 export default function Home () {
   return (
     <ScrollView style={styles.container}>
-
       <Image
         style={styles.img}
         source={{
           uri: 'https://m.media-amazon.com/images/G/32/SellerUniversity/SU2020/SellerApp/Selldot/833_f21_banner-grid2_01_1500x1025._SL1280_FMjpg_.jpg',
         }}/>
       <Text style={styles.text}>Clique para ver looks para inverno</Text>
-
       <Image
         style={styles.img}
         source={{
           uri: 'https://img.freepik.com/fotos-premium/ferias-de-verao-viagem-conceito-plana-leigos-acessorios-de-praia_72402-550.jpg?w=2000',
         }}/>
       <Text style={styles.text}>Clique para ver looks para verão</Text>
-
       <Image
         style={styles.img}
         source={{
           uri: 'https://img.freepik.com/fotos-premium/outono-ainda-vida-folhas-amarelas-abobora-velas-sueter-tricotado-em-uma-vista-superior-branca_251908-17.jpg',
         }}/>
       <Text style={styles.text}>Clique para ver looks para outono</Text>
-
       <Image
         style={styles.img}
         source={{
           uri: 'https://www.interlagos.com.br/blog/wp-content/uploads/2021/04/Home-Blog-Looks-B%C3%A1sicos-1140x853.jpeg',
         }}/>
       <Text style={styles.text}>Clique para ver looks para primavera</Text>
-
     </ScrollView>
   );
 };
